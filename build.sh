d7c6e2e07568d1c7581a6d85f0d181ccbdd22471
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-13 14:13:42 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,14 +11,13 @@ rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklis
 # Install hyprland
 curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-40/solopasha-hyprland-fedora-40.repo
 rpm-ostree refresh-md --force
-rpm-ostree hyprland 
+rpm-ostree hyprland waybar flatpak
 
 # Install greetd / tuigreet
 rpm-ostree greetd tuigreet
+curl --output-dir "/etc/greetd/" --remote-name https://raw.githubusercontent.com/pande2929/pande-blue/main/config.toml
+systemctl enable greetd
 
 # Install virtualization
 rpm-ostree install virt-manager
 systemctl enable libvirtd
-
-# Remove firefox (use flathub version instead)
-rpm-ostree override remove firefox firefox-langpacks
