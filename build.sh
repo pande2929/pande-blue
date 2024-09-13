e5a7acb621b3a2d1569b78749493b922ce053cf2
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-13 12:03:50 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,17 @@ RELEASE="$(rpm -E %fedora)"
 # Install nvidia-drivers
 rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia
 rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1 --append=initcall_blacklist=simpledrm_platform_driver_init
+
+# Install LXQT
+rpm-ostree install breeze-cursor-theme breeze-gtk breeze-icon-theme dnfdragora-updater falkon firewall-config initial-setup-gui \
+  lxqt-about lxqt-archiver lxqt-config lxqt-globalkeys lxqt-notificationd lxqt-openssh-askpass lxqt-panel lxqt-policykit \ 
+  lxqt-powermanagement lxqt-qtplugin lxqt-runner lxqt-session lxqt-themes lxqt-themes-fedora network-manager-applet nm-connection-editor \ 
+  notification-daemon obconf openbox pcmanfm-qt perl-File-MimeInfo qterminal sddm sddm-themes sddm-x11 upower xdg-user-dirs --allow-inactive
+
+# Install virtualization
+rpm-ostree install virt-manager
+systemctl enable libvirtd
+
+# Remove firefox (use flathub version instead)
+rpm-ostree override remove firefox firefox-langpacks
+
