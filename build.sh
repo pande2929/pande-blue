b5e4c7b4ff8edc095d8d7a9eb1b46e71fcc306f6
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-13 16:34:14 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ RELEASE="$(rpm -E %fedora)"
 # Install hyprland
 curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-40/solopasha-hyprland-fedora-40.repo
 #rpm-ostree refresh-md --force
-rpm-ostree hyprland waybar flatpak
+rpm-ostree install hyprland waybar flatpak
 
 # Install greetd / tuigreet
 rpm-ostree greetd tuigreet
