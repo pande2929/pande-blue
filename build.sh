db294230fc9a103cd67c68506380a935ceaeac82
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-13 11:24:20 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Install nvidia-drivers
-rpm-ostree install kmod-nvidia xorg-x11-drv-nvidia
+rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia
 rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1 --append=initcall_blacklist=simpledrm_platform_driver_init
