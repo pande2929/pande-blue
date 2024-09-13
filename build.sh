2b9cab874629514487e7d33f70c21889b0144044
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-13 16:16:14 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Install nvidia-drivers
-rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia
-rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1
+#rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia
+#rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1
 
 # Install hyprland
 curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-40/solopasha-hyprland-fedora-40.repo
