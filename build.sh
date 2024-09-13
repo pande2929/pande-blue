8a160c44d58c23ef0c0919a1b65fc59efa0c5bcb
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-13 12:55:19 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ RELEASE="$(rpm -E %fedora)"
 rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia
 rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1 --append=initcall_blacklist=simpledrm_platform_driver_init
 
-# Install LXQT
-rpm-ostree install breeze-cursor-theme breeze-gtk breeze-icon-theme dnfdragora-updater falkon firewall-config initial-setup-gui \
-  lxqt-about lxqt-archiver lxqt-config lxqt-globalkeys lxqt-notificationd lxqt-openssh-askpass lxqt-panel lxqt-policykit \ 
-  lxqt-powermanagement lxqt-qtplugin lxqt-runner lxqt-session lxqt-themes lxqt-themes-fedora network-manager-applet nm-connection-editor \ 
-  notification-daemon obconf openbox pcmanfm-qt perl-File-MimeInfo qterminal sddm sddm-themes sddm-x11 upower xdg-user-dirs --allow-inactive
+# Install hyprland
+curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-40/solopasha-hyprland-fedora-40.repo
+rpm-ostree refresh-md --force
+
+# Install greetd / tuigreet
 
 # Install virtualization
 rpm-ostree install virt-manager
@@ -20,4 +20,3 @@ systemctl enable libvirtd
 
 # Remove firefox (use flathub version instead)
 rpm-ostree override remove firefox firefox-langpacks
-
