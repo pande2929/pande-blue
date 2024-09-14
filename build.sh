bbff13a6b4737136b18b2892ad9de26479b3b403
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-13 18:33:47 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,10 +8,9 @@ RELEASE="$(rpm -E %fedora)"
 curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-40/solopasha-hyprland-fedora-40.repo
 
 # Install our software
-rpm-ostree install hyprland waybar flatpak sddm virt-manager alacritty
+rpm-ostree install hyprland waybar flatpak sddm virt-manager alacritty gamemode vim neovim pavucontrol btop rofi-wayland
+rpm-ostree remove kitty
 
 # Finalize configuration
-#curl --output-dir "/etc/greetd/" --remote-name https://raw.githubusercontent.com/pande2929/pande-blue/main/config.toml
-#systemctl enable greetd
 systemctl enable sddm
 systemctl enable libvirtd
