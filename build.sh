24a41fa7ebd22724f7b4c216a452bf0bd750ee4e
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-13 19:40:58 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ RELEASE="$(rpm -E %fedora)"
 curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-40/solopasha-hyprland-fedora-40.repo
 
 # Install our software
-rpm-ostree install hyprland waybar flatpak sddm virt-manager alacritty gamemode vim neovim pavucontrol btop rofi-wayland pcmanfm-qt \
+rpm-ostree install hyprland flatpak sddm virt-manager alacritty gamemode vim neovim pavucontrol btop rofi-wayland pcmanfm-qt \
   hyprpaper hyprlock hypridle xdg-desktop-portal-hyprland waybar-git hyprcursor hyprland-plugins-git hyprshot
 
 # Finalize configuration
