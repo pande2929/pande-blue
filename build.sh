bb5023c2bf8ab92d33f1e3844f6912fa4303d49e
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-16 21:04:52 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,10 @@ curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfraclou
 
 # Install our software
 rpm-ostree install hyprland-git flatpak sddm virt-manager alacritty gamemode vim neovim pavucontrol btop rofi-wayland thunar \
-  hyprpaper hyprlock hypridle xdg-desktop-portal-hyprland waybar-git hyprcursor hyprland-plugins-git hyprshot cmake meson cpio
+  hyprpaper hyprlock hypridle xdg-desktop-portal-hyprland waybar-git hyprcursor hyprland-plugins-git hyprshot cmake meson cpio \
+  gnu-free-fonts-common gnu-free-mono-fonts gnu-free-sans-fonts gnu-free-serif-fonts
+
+
 
 # Finalize configuration
 systemctl enable sddm
