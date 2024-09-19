fa6cb3c27d9802ad795d0f0ad5f8a96fc7b26ba4
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-19 13:27:04 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,11 @@ RELEASE="$(rpm -E %fedora)"
 # Enable Hyprland
 curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-40/solopasha-hyprland-fedora-40.repo
 
+rpm-ostree cleanup -m
+rpm-ostree refresh-md
+
 # Install our software
-rpm-ostree install hyprland flatpak sddm virt-manager alacritty gamemode vim neovim pavucontrol btop rofi-wayland thunar \
+rpm-ostree install hyprland-git flatpak sddm virt-manager alacritty gamemode vim neovim pavucontrol btop rofi-wayland thunar \
   hyprpaper hyprlock hypridle xdg-desktop-portal-hyprland waybar-git hyprcursor hyprshot \
   gnu-free-fonts-common gnu-free-mono-fonts gnu-free-sans-fonts gnu-free-serif-fonts
 
