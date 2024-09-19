2e2d32693fe733259070a9ab478a0e2307fbf82b
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-19 13:13:43 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfraclou
 
 # Install our software
 rpm-ostree install hyprland-git flatpak sddm virt-manager alacritty gamemode vim neovim pavucontrol btop rofi-wayland thunar \
-  hyprpaper hyprlock hypridle xdg-desktop-portal-hyprland waybar-git hyprcursor hyprland-plugins-git hyprshot \
+  hyprpaper hyprlock hypridle xdg-desktop-portal-hyprland waybar-git hyprcursor hyprshot \
   gnu-free-fonts-common gnu-free-mono-fonts gnu-free-sans-fonts gnu-free-serif-fonts
 
 # Finalize configuration
