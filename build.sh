ce9d4d9f1bce6753e9578faf8b1b31cffae51185
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-19 09:28:13 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Enable Hyprland
-curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-40/solopasha-hyprland-fedora-40.repo
+#curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-40/solopasha-hyprland-fedora-40.repo
 
 # Install our software
-rpm-ostree install hyprland-git flatpak sddm virt-manager alacritty gamemode vim neovim pavucontrol btop rofi-wayland thunar \
-  hyprpaper hyprlock hypridle xdg-desktop-portal-hyprland waybar-git hyprcursor hyprland-plugins-git hyprshot cmake meson cpio \
-  gnu-free-fonts-common gnu-free-mono-fonts gnu-free-sans-fonts gnu-free-serif-fonts
+#rpm-ostree install hyprland-git flatpak sddm virt-manager alacritty gamemode vim neovim pavucontrol btop rofi-wayland thunar \
+#  hyprpaper hyprlock hypridle xdg-desktop-portal-hyprland waybar-git hyprcursor hyprland-plugins-git hyprshot cmake meson cpio \
+#  gnu-free-fonts-common gnu-free-mono-fonts gnu-free-sans-fonts gnu-free-serif-fonts
 
 # Finalize configuration
-systemctl enable sddm
-systemctl enable libvirtd
+#systemctl enable sddm
+#systemctl enable libvirtd
