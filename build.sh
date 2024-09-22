dd715c3696428b1699324992eedccba5aa03e20a
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-22 11:22:52 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,17 +5,17 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Enable fysnc copr
-#curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/repo/fedora-40/sentry-kernel-fsync-fedora-40.repo
-#echo exclude=kernel* >> /etc/yum.repos.d/fedora-updates
+curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/repo/fedora-40/sentry-kernel-fsync-fedora-40.repo
+echo exclude=kernel* >> /etc/yum.repos.d/fedora-updates
 
 #rpm-ostree cleanup -m
 
 # Install our software
-rpm-ostree install mangohud virt-manager
+rpm-ostree install mangohud virt-manager btop
 
 # Install fsync kernel
 # https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/
-#rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-fsync' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-fsync' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 
 # Complete configuration
 systemctl enable libvirtd
