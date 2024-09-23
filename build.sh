ca7b12b751820f254963ac4434d566d23481ea8c
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-22 18:53:53 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ RELEASE="$(rpm -E %fedora)"
 curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/repo/fedora-40/sentry-kernel-fsync-fedora-40.repo
 echo exclude=kernel* >> /etc/yum.repos.d/fedora-updates
 
-#rpm-ostree cleanup -m
+rpm-ostree cleanup -m
 
 # Install our software
-rpm-ostree install mangohud virt-manager btop #akmod-nvidia xorg-x11-drv-nvidia-cuda
+rpm-ostree install mangohud virt-manager btop akmod-nvidia xorg-x11-drv-nvidia
 
 # Install fsync kernel
 # https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/
