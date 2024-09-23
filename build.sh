d2f00d0571f6b49e1d5a9b59c7e336229db4b1e3
Tarnished Millennial <98792311+pande2929@users.noreply.github.com>
2024-09-22 19:36:08 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,17 +5,17 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Enable fysnc copr
-curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/repo/fedora-40/sentry-kernel-fsync-fedora-40.repo
-echo exclude=kernel* >> /etc/yum.repos.d/fedora-updates
+#curl --output-dir "/etc/yum.repos.d/" --remote-name https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/repo/fedora-40/sentry-kernel-fsync-fedora-40.repo
+#echo exclude=kernel* >> /etc/yum.repos.d/fedora-updates
 
-rpm-ostree cleanup -m
+#rpm-ostree cleanup -m
 
 # Install our software
-rpm-ostree install mangohud virt-manager btop kmod-nvidia xorg-x11-drv-nvidia
+rpm-ostree install mangohud virt-manager btop #kmod-nvidia xorg-x11-drv-nvidia
 
 # Install fsync kernel
 # https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/
-rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-fsync' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-devel
+#rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-fsync' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-devel
 
 # Complete configuration
 systemctl enable libvirtd
